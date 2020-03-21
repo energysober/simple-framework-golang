50f59cada6ac00db79cb160d243560d9761b78bf
energysober <luokaibinenergy@gmail.com>
2020-03-21 22:53:18 +0800

    add cache mutex

---
+++ go-cache/byteview.go
@@ -0,0 +1,27 @@
+package go_cache
+
+// ByteView a byte view holds an immutable view of bytes
+type ByteView struct {
+	b []byte
+}
+
+// Len
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+// ByteSlice return a copy of data as a byte slice
+func (v ByteView) ByteSlice() []byte {
+	return cloneByte(v.b)
+}
+
+// String return data as a string
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneByte(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
