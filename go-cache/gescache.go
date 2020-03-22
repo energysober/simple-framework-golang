468959aca5976a0ac65cfe8fb815629ee5a40f88
energysober <luokaibinenergy@gmail.com>
2020-03-22 21:56:52 +0800

    add cache group When the cache is not found, re-fetch the data

---
+++ go-cache/gescache.go
@@ -0,0 +1,83 @@
+package go_cache
+
+import (
+	"fmt"
+	"sync"
+)
+
+// A Getter loads data for a key
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// A GetterFunc implements Getter with a function
+type GetterFunc func(key string) ([]byte, error)
+
+// Get a implements function
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+// A Group is a cache namespace and associated data load spread over
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+// New a Group, append new group to groups
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+// Get a Group
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	g := groups[name]
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		return v, nil
+	}
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (ByteView, error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneByte(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
---
+++ go-cache/gescache_test.go
@@ -0,0 +1,52 @@
+package go_cache
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestGetter(t *testing.T) {
+	var f Getter = GetterFunc(func(key string) ([]byte, error) {
+		return []byte(key), nil
+	})
+
+	expect := []byte("key")
+	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
+		t.Fatal("callback failed")
+	}
+}
+
+var db = map[string]string{
+	"Windy": "100",
+	"Mack":  "99",
+	"Stone": "80",
+}
+
+func TestGet(t *testing.T) {
+	loadCounts := make(map[string]int, len(db))
+
+	ges := NewGroup("scores", 2<<10, GetterFunc(func(key string) ([]byte, error) {
+		if value, ok := db[key]; ok {
+			if _, ok := loadCounts[key]; !ok {
+				loadCounts[key] = 1
+			}
+			loadCounts[key] += 1
+			return []byte(value), nil
+		}
+		return nil, fmt.Errorf("%s not exist", key)
+	}))
+
+	for k, v := range db {
+		if view, err := ges.Get(k); err != nil || view.String() != v {
+			t.Fatalf("failed to get value of %s", v)
+		}
+		if _, err := ges.Get(k); err != nil || loadCounts[k] > 1 {
+			t.Fatalf("cache %s miss", k)
+		}
+	}
+
+	if view, err := ges.Get("unknown"); err == nil {
+		t.Fatalf("the value of unknow should be empty, but %s got", view)
+	}
+}
