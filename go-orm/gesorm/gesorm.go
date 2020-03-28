eec6c80c0a7abe3fb68e93f8b160c2917e1e3001
energysober <luokaibinenergy@gmail.com>
2020-03-28 17:16:32 +0800

    add go orm engine

--- go-orm/gesorm/gesorm.go
+++ go-orm/gesorm/gesorm.go
@@ -1,1 +1,38 @@
 package gesorm
+
+import (
+	"database/sql"
+	"github.com/simple-framework-golang/go-orm/gesorm/log"
+	"github.com/simple-framework-golang/go-orm/gesorm/session"
+)
+
+// Engine db engine
+type Engine struct {
+	db *sql.DB
+}
+
+// New Engine
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	e = &Engine{db: db}
+	log.Info("Connect database success")
+	return
+}
+
+// Close database
+func (engine *Engine) Close() {
+	if err := engine.db.Close(); err != nil {
+		log.Errorf("Failed to close database: %s", err)
+	}
+	log.Info("Close database success")
+}
+
+// NewSession
+func (engine *Engine) NewSession() *session.Session {
+	return session.New(engine.db)
+}
